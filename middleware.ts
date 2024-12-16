dd9b667fcfa170f796d4f9901488725eddefe857
Anurag <anuragma807@gmail.com>
2024-12-16 17:43:34 +0530

    added code for vercel

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
-import { authMiddleware } from "@clerk/nextjs";
+// import { authMiddleware } from "@clerk/nextjs";
 import { NextRequest, NextResponse } from "next/server";
  
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
-export default authMiddleware({
-  publicRoutes: ["/api/webhook","/api/uploadthing"]
-});
+// export default authMiddleware({
+//   publicRoutes: ["/api/webhook","/api/uploadthing"]
+// });
 // export function middleware(req: NextRequest) {
 //   const url = req.nextUrl.clone();
 
